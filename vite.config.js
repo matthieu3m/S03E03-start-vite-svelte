c12a9999cfebb49465ee23cde83eea70c51f76f2
MECHAT Matthieu <mechat.m@icloud.com>
2024-10-23 14:21:15 +0200

    add .env .env.production .enve.example

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { build, defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {visualizer} from 'rollup-plugin-visualizer'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    visualizer({
+      emitFile: true, // crée le fichier dabs dist (sinon il va dans la racine ce qui n'est pas top)
+      sourcemap: true,
+    })
+  ],
+
+  build: {
+    sourcemap: true,
+  }
+})
+
+ 
